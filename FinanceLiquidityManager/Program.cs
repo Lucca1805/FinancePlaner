f0c7c13fb55440ba632073242062d15f672871a7
Diana Pazer <dp27801@outlook.de>
2024-05-25 10:23:07 +0200

    added swagger token

--- FinanceLiquidityManager/Program.cs
+++ FinanceLiquidityManager/Program.cs
@@ -7,6 +7,7 @@ using FinanceLiquidityManager.Infrastructure.Person;
 using FinanceLiquidityManager.Infrastructure.Transaction;
 using FinanceLiquidityManager.Infrastructure.Credit;
 using FinanceLiquidityManager.Infrastructure.File;
+using Microsoft.OpenApi.Models;
 //using FinanceLiquidityManager.Infrastructure.Loan;
 
 
@@ -42,6 +43,40 @@ builder.Services.AddAuthentication(opt =>
         ValidIssuer = jwtSettings["Issuer"],
         ValidAudience = jwtSettings["Audience"]
     };
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "JWTToken_Auth_API",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
 builder.Services.AddCors(options =>
 {
