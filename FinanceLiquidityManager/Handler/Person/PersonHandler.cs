35aeba66725165be6e970c6ddaaec6bff75fca6e
Lucca1805 <l.leserer@t-online.de>
2024-06-07 10:32:51 +0200

    Once a User is updated a new Token is generated -> Fixes Issue with missing update info

--- FinanceLiquidityManager/Handler/Person/PersonHandler.cs
+++ FinanceLiquidityManager/Handler/Person/PersonHandler.cs
@@ -10,7 +10,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
-
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Data;
+using Microsoft.AspNetCore.Authorization;
+using System.Text;
+using FinanceLiquidityManager.Controllers;
 namespace FinanceLiquidityManager.Handler.Person
 {
     public class PersonHandler
@@ -68,7 +74,26 @@ namespace FinanceLiquidityManager.Handler.Person
 
                 if (result > 0)
                 {
-                    return new OkObjectResult(new { Message = "User updated successfully." });
+                    var claims = new[]
+                    {
+                            new Claim(JwtRegisteredClaimNames.Sub, request.UserName),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim("CurrencyPreference",request.CurrencyPreference),
+                            new Claim("UserId", request.PersonId.ToString())
+                        };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    //var expiry = DateTime.Now.AddMinutes(30);
+                    var expiry = DateTime.UtcNow.AddYears(1);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: expiry,
+                        signingCredentials: creds
+                    );
+                    return new OkObjectResult(new { Message = "User udpated successfully",token =  new JwtSecurityTokenHandler().WriteToken(token)});
                 }
                 else
                 {
@@ -390,7 +415,7 @@ INSERT INTO finance.transactions (
                 };
                 await connection.ExecuteAsync(standingOrderQuery, standingOrderParameters3);
                 await connection.ExecuteAsync(standingOrderQuery, standingOrderParameters4);
-              decimal previousBalance2 = 3500.00m; 
+                decimal previousBalance2 = 3500.00m;
                 for (int i = 0; i < 6; i++)
                 {
                     int transactionAmount = 0;
